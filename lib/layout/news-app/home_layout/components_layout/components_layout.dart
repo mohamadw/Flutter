d8b86dd12f3666e7b24b85b39d44e0ae53a396dc
mohamadw <55956413+mohamadw@users.noreply.github.com>
2021-09-04 08:33:32 +0300

    now the navigator bar is run

---
+++ lib/layout/news-app/home_layout/components_layout/components_layout.dart
@@ -0,0 +1,20 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:news_app/layout/news-app/cubit/cubit_layout.dart';
+
+
+MyBottomNavigationBar(context) {
+  var cubit  = CubitNewsLayout.get(context);
+
+  return BottomNavigationBar(
+    currentIndex: cubit.current_index,
+    onTap: (index){cubit.ChangeNavigatorBar(index);},
+    items: [
+      BottomNavigationBarItem(
+          icon: Icon(Icons.sports_basketball), label: 'sports'),
+      BottomNavigationBarItem(icon: Icon(Icons.business), label: 'business'),
+      BottomNavigationBarItem(icon: Icon(Icons.science), label: 'science'),
+    ],
+  );}
--- lib/layout/news-app/home_layout/home_layout.dart
+++ lib/layout/news-app/home_layout/home_layout.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/shared/components/components.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/layout/news-app/cubit/cubit_layout.dart';
+import 'package:news_app/layout/news-app/states/cubit_states_layout.dart';
+import 'components_layout/components_layout.dart';
 
 class HomeLayout extends StatelessWidget {
   const HomeLayout({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('NewsApp'),
-      ),
-      body: Text('Home'),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          NavigatorBarItem(icon:Icons.sports_basketball , label:'sports' ),
-          NavigatorBarItem(icon:Icons.business , label:'business' ),
-          NavigatorBarItem(icon:Icons.science , label:'science' ),
-        ],
-      ),
+
+    return BlocProvider(
+        create: (BuildContext context) {
+          return CubitNewsLayout();
+        },
+
+      child:BlocConsumer<CubitNewsLayout,NewsLayoutStates>(
+        listener:(context , state) {} ,
+        builder:(context , state){
+          var cubit  = CubitNewsLayout.get(context);
+
+          return Scaffold(
+              appBar: AppBar(
+                title: Text('NewsApp'),
+              ),
+              body: Text('Home'),
+              bottomNavigationBar:MyBottomNavigationBar(context)
+          );
+
+        } ,
+      )
     );
   }
 }
