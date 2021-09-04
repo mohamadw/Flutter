39202e71888a12a5e0790d7d62678b6f9187e04f
mohamadw <55956413+mohamadw@users.noreply.github.com>
2021-09-04 08:59:47 +0300

    add app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:news_app/layout/news-app/home_layout/home_layout.dart';
 import 'package:news_app/shared/bloc_observer.dart';
 
@@ -13,9 +14,31 @@ class NewsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeLayout(),
       theme: ThemeData(
-        appBarTheme: AppBarTheme(color:Colors.red)
+        appBarTheme: AppBarTheme(
+          color: Colors.deepOrangeAccent,
+          backwardsCompatibility: false,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarColor: Colors.deepOrangeAccent,
+            statusBarIconBrightness: Brightness.light,
+          ),
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.deepOrangeAccent,
+          type: BottomNavigationBarType.fixed,
+        ),
+        scaffoldBackgroundColor: Colors.white,
       ),
     );
   }
